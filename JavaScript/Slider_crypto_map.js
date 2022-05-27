ea82bd9f93227ab1a10d756b4d6b270ac231d27f
Yason <nazardenr@mail.ru>
2022-05-28 01:53:38 +0300

    1

--- JavaScript/Slider_crypto_map.js
+++ JavaScript/Slider_crypto_map.js
@@ -5,14 +5,26 @@ var slide_third = document.querySelector('.crypto_map_number_slider_third')
 var slide_fourth = document.querySelector('.crypto_map_number_slider_fourth')
 var active = document.querySelector('.active')
 slide_first.onclick = function(){
-    slide_first.classList.toggle('active')
+    slide_first.classList.add('active')
+    slide_second.classList.remove('active')
+    slide_third.classList.remove('active')
+    slide_fourth.classList.remove('active')
 }
-slide_first.onclick = function(){
-    slide_second.classList.toggle('active')
+slide_second.onclick = function(){
+    slide_second.classList.add('active')
+    slide_first.classList.remove('active')
+    slide_third.classList.remove('active')
+    slide_fourth.classList.remove('active')
 }
 slide_third.onclick = function(){
-    slide_first.classList.toggle('active')
+    slide_third.classList.add('active')
+    slide_first.classList.remove('active')
+    slide_second.classList.remove('active')
+    slide_fourth.classList.remove('active')
 }
 slide_fourth.onclick = function(){
-    slide_first.classList.toggle('active')
+    slide_fourth.classList.add('active')
+    slide_first.classList.remove('active')
+    slide_second.classList.remove('active')
+    slide_third.classList.remove('active')
 }
