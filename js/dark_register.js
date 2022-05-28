e3eddfb7d2d2fd3a27694a166cd14749070356a0
Maritxuak <97700561+Maritxuak@users.noreply.github.com>
2022-05-28 22:10:45 +0300

    ол

--- js/dark_register.js
+++ js/dark_register.js
@@ -1,11 +1,12 @@
-    let switchMode = document.getElementById("moon2");
+    let switchMode = document.getElementById("monk");
 
     switchMode.onClick = function() {
+        
         let theme = document.getElementById("mon");
 
         if (theme.getAttribute("href") == "../dark css/register.css"){
-        theme.href = "../css/register.css";
+            theme.href = "../css/register.css";
         }  else {
-            theme.href = "../dark css/register.css"
+            theme.href = "../dark css/register.css";
         }
     }
