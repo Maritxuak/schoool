1edd80caa70e31f1b091130de95a9f385f31fe20
Yason <nazardenr@mail.ru>
2022-05-31 21:19:42 +0300

    1

--- js/slider_fact.js
+++ js/slider_fact.js
@@ -3,4 +3,5 @@ var s2 = document.querySelector('.slider_fact_second')
 var s3 = document.querySelector('.slider_fact_third')
 var s4 = document.querySelector('.slider_fact_fourth')
 var s5 = document.querySelector('.slider_fact_fifth')
-var 
+var ar_top = document.querySelector('.navigation_top')
+var ar_down = document.querySelector('.navigation_down')
