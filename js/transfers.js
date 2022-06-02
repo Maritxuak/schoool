3510a8237afea4701c4b046d4cac217e1e1059fb
Yason <nazardenr@mail.ru>
2022-06-02 22:32:36 +0300

    1

---
+++ js/transfers.js
@@ -0,0 +1,10 @@
+var sl1 = document.querySelector('.slot_1')
+var sl2 = document.querySelector('.slot_2')
+var sl3 = document.querySelector('.slot_3')
+var sl4 = document.querySelector('.slot_4')
+var sl5 = document.querySelector('.slot_5')
+var sl6 = document.querySelector('.slot_6')
+var sl7 = document.querySelector('.slot_7')
+sl1.onclick = function(){
+    sl1.classList.toggle('activation')
+}
