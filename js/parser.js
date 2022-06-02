1770cb704c93b626f4190b241b3fbb6263fc526c
Yason <nazardenr@mail.ru>
2022-06-02 13:31:01 +0300

    1

--- js/parser.js
+++ js/parser.js
@@ -1,11 +1,38 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
+const versionSW = "30January009999";
 
-axios.get('').then(html => {
-    const $ = cheerio.load(html.data)
-    let text = ''
-    $('#bloktxt > h4').each((i, elem) => {
-        text += `${$(elem).text()}\n`
-    })
-    console.log(text);
-})
+const CACHE_NAME = "V9";
+const STATIC_CACHE_URLS = [
+  "/",
+  "sw.js",
+  "/icons/binance_svg.svg",
+  "/icons/ftx_svg.svg",
+  "/icons/logo.svg",
+  "/icons/logo-192.png",
+  "/icons/logo-512.png"
+];
+
+self.addEventListener("install", event => {
+  console.log("Service Worker installing.");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_CACHE_URLS))
+    );
+});
+
+self.addEventListener("activate", event => {
+  console.log("Service Worker activating.");
+});
+
+self.addEventListener("fetch", event => {
+    console.log(`Request of ${event.request.url}`);
+  // default behaviour: request the network
+  event.respondWith(fetch(event.request));
+});
+
+Cache-First Strategy
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches
+      .match(event.request) // check if the request has already been cached
+      .then(cached => cached || fetch(event.request)) // otherwise request network
+  );
+});
