6d6f5aabca647bc53d6267b9ffb986ca6278dbf6
Maritxuak <97700561+Maritxuak@users.noreply.github.com>
2022-06-21 23:50:05 +0300

    f

---
+++ js/AGREEMENT.js
@@ -0,0 +1,33 @@
+function krestik(){
+    document.getElementById("wrapper_okno").classList.remove("zaktito")
+    document.getElementById("wrapper2_zak").classList.remove("close")
+}
+
+function spisok(){
+    document.getElementById("xoxol").classList.toggle("xaxol")
+    document.getElementById("xoxol2").classList.toggle("xaxol")
+    document.getElementById("xoxol3").classList.toggle("xaxol")
+    
+}
+
+function spisok2(){
+    document.getElementById("kykold").classList.toggle("xaxol2")
+    document.getElementById("xoxol2").classList.toggle("xaxol2")
+    document.getElementById("xoxol3").classList.toggle("xaxol2")
+}
+
+function spisok3(){
+    document.getElementById("kykold").classList.toggle("xaxol2")
+    document.getElementById("xoxol").classList.toggle("xaxol2")
+    document.getElementById("xoxol3").classList.toggle("xaxol2")
+}
+
+function spisok4(){
+    document.getElementById("kykold").classList.toggle("xaxol2")
+    document.getElementById("xoxol2").classList.toggle("xaxol2")
+    document.getElementById("xoxol").classList.toggle("xaxol2")
+}
+function Burger(){
+    document.getElementById("wrapper_okno").classList.toggle("zaktito")
+    document.getElementById("wrapper2_zak").classList.toggle("close")
+}
